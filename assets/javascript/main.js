a8d80a91cc8317d148b911bcf9a374683b68f356
kanikas01 <larvakurt@yahoo.com>
2019-01-31 11:54:36 -0800

    Add basic event listeners

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,7 +16,8 @@ $(document).ready(function () {
 
   // Selectors
   var selectors = {
-    button: "#add-train-btn"
+    button: "#add-train-btn",
+    trainTable: "#train-table"
   };
 
   // Classes
@@ -25,8 +26,48 @@ $(document).ready(function () {
   // Events
 
   $(selectors.button).on("click", function(event) {
-    console.log($(selectors.button));
     event.preventDefault();
+
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var firstTrainTime = moment($("#first-train-time-input").val().trim(), "HH:mm").format("X");
+    var trainFrequency = $("#frequency-input").val().trim();
+
+    // Creates temporary object for holding train data
+    var newTrain = {
+      train: trainName,
+      destination: trainDestination,
+      firstJourney: firstTrainTime,
+      frequency: trainFrequency
+    };
+
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-time-input").val("");
+    $("#frequency-input").val("");
+  });
+
+  database.ref().on("child_added", function (snapshot) {
+    var train = snapshot.val().train;
+    var destination = snapshot.val().destination;
+    var firstJourney = snapshot.val().firstJourney;
+    var frequency = snapshot.val().frequency;
+
+    var newRow = $("<tr><td>" + train + "</td>" +
+      "<td>" + destination + "</td>" +
+      "<td>" + frequency + "</td>" +
+      "<td>" + "Some value" + "</td>" +
+      "<td>" + "Some other value" + "</td></tr>");
+
+    $(selectors.trainTable).append(newRow);
+
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
   });
 
   // Helpers
